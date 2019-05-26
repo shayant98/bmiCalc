cb2595b0ee79e74e90525d0dc34b78939626934b
Shayant Sital <shayant@gmail.com>
2019-05-26 01:48:54 -0300

    added function to get all users

--- src/main/java/sr/unasat/bmi/calculator/app/Application.java
+++ src/main/java/sr/unasat/bmi/calculator/app/Application.java
@@ -1,9 +1,13 @@
 package sr.unasat.bmi.calculator.app;
 
-import sr.unasat.bmi.calculator.repositories.BmiLogRepository;
+import sr.unasat.bmi.calculator.repositories.UserRepository;
 
 public class Application {
     public static void main(String[] args) {
-        BmiLogRepository bmiLogRepository = new BmiLogRepository();
+        UserRepository userRepository = new UserRepository();
+
+        //test db function
+       userRepository.findAllUsers().forEach(user -> System.out.println(user.getName()));
+
     }
 }
--- src/main/java/sr/unasat/bmi/calculator/repositories/UserRepository.java
+++ src/main/java/sr/unasat/bmi/calculator/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package sr.unasat.bmi.calculator.repositories;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import sr.unasat.bmi.calculator.entities.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserRepository {
     private Connection connection;
@@ -21,5 +23,27 @@ public class UserRepository {
         }catch (SQLException execption){
             execption.printStackTrace();
         }
+    }
+
+
+    public List<User> findAllUsers() {
+        List<User> UserList = new ArrayList<>();
+        Statement stmt;
+        try {
+            stmt = connection.createStatement();
+            String sql = "select * from users";
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("resultset: " + rs);
+            //STEP 5: Extract data from result set
+            while (rs.next()) {
+                UserList.add(new User(rs.getInt("id"), rs.getString("name"), rs.getString("surname"), rs.getString("username"), rs.getString("name"), rs.getInt("height")));
+            }
+            rs.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return UserList;
     }
 }
